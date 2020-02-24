2fa0a610a63c88d7aad78a300d8415d5fc74ccf4
tonyCodeMaster <61329413+tonyCodeMaster@users.noreply.github.com>
2020-02-24 21:28:57 +0200

    asd

---
+++ PiP/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class GroundCheck : MonoBehaviour
+    {
+        GameObject Player;
+        void Start()
+        {
+            Player = gameObject.transform.parent.gameObject;
+        }
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.collider.tag == "Ground")
+            {
+                Player.GetComponent<PlayerMovement>().isGrounded = true;
+            }
+        }
+
+        void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.collider.tag == "Ground")
+            {
+                Player.GetComponent<PlayerMovement>().isGrounded = false;
+            }
+        }
+    }
+}
---
+++ PiP/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public bool isGrounded = false;
+    public Vector2 jumpVector = new Vector2 (0f,10f);
+    void Update()
+    {
+        
+        // Get the horizontal and vertical axis.
+        // By default they are mapped to the arrow keys.
+        // The value is in the range -1 to 1
+        float leftRight = Input.GetAxis("Horizontal") * speed;
+
+
+        // Make it move 10 meters per second instead of 10 meters per 
+
+frame...
+        leftRight *= Time.deltaTime;
+
+        // Move translation along the object's z-axis
+        transform.Translate(leftRight, 0, 0);
+        Jump();
+
+    }
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(jumpVector, 
+
+ForceMode2D.Impulse);
+        }
+    }
+
+    
+}*/
+public class PlayerMovement : MonoBehaviour
+{
+    // paramater of speed variable
+    public float speed = 10;
+    public Vector2 jumpVector = new Vector2(0f, 10f);
+    public bool isGrounded = false;
+    void Update()
+    {
+        // making direction by getting speed multiplyed with -1 or 1 ;
+        float leftRight = Input.GetAxis("Horizontal") * speed;
+
+        // sync it to frame per second
+        leftRight *= Time.deltaTime;
+
+        // Move translation along the object's z-axis
+        transform.Translate(leftRight, 0, 0);
+
+        Jump();
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+        {
+            gameObject.GetComponent<Rigidbody2D>().AddForce(jumpVector,ForceMode2D.Impulse);
+        }
+    }
+}
