1664c5a3a55bb03d1f511c195afa51673516b9c4
Eduard <bakayaro.ed@gmail.com>
2020-02-26 19:54:06 +0200

    updating my project

---
+++ PiP/Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class GroundCheck : MonoBehaviour
+    {
+        GameObject Player;
+        void Start()
+        {
+            Player = GameObject.Find("Player");
+        }
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.collider.tag == "Ground")
+            {
+                Player.GetComponent<PlayerMovement>().isGrounded = true;
+            }
+        }
+
+        void OnCollisionExit2D(Collision2D collision)
+        {
+            if (collision.collider.tag == "Ground")
+            {
+                Player.GetComponent<PlayerMovement>().isGrounded = false;
+            }
+        }
+    }
+}
