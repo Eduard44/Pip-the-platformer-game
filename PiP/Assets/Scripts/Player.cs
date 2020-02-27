9fcd52a90a72d34ee3844ebd5c6bcc6869e4e4c8
Eduard <bakayaro.ed@gmail.com>
2020-02-27 20:32:32 +0200

    changed how to rotate
    changed how to rotate the player and the speed of the idle pose

--- PiP/Assets/Scripts/Player.cs
+++ PiP/Assets/Scripts/Player.cs
@@ -7,21 +7,17 @@ public class Player : MonoBehaviour
     // this is to understand where the mouse to look at;
     private Vector2 lookDirection;
 
-    //this is the player SpriteRenderer
-    private SpriteRenderer spriteRenderer;
-
     //this initializes things that go on starting the game
     private void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
 
         //to not destroy the player loading a new level
         DontDestroyOnLoad(gameObject);
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        //the direction of the mouse
+        //the direction of the mouse wherever it moves
         lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         RotatePlayer(lookDirection);
     }
@@ -29,17 +25,17 @@ public class Player : MonoBehaviour
     void RotatePlayer(Vector2 lookDirection)
     {
         Vector2 playerPosition = gameObject.transform.position;
-        //if the mouse looks towards the right of the player or above him we don't turn
-        //ergo false flipX in the SpriteRenderer
+        //if the mouse looks towards the right of the player or above him
+        //we rotate to the right
         if (lookDirection.x >= playerPosition.x)
         {
-            spriteRenderer.flipX = false;
+            transform.localScale = Vector2.one;
         }
-        //if the mouse looks to the left of the player we turn 
-        //ergo true flipX in the SpriteRenderer
+        //if the mouse looks to the left of the player 
+        //we rotate to the left
         else if (lookDirection.x < playerPosition.x)
         {
-            spriteRenderer.flipX = true;
+            transform.localScale = new Vector2(-1, 1);
         }
     }
 }
